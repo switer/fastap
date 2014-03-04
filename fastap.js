91eb6e644b2c5dfd832a2bb80abf0d8090a6a6e8
switer <guankaishe@gmail.com>
2014-03-04 16:11:49 +0800

    touchable detect;

--- fastap.js
+++ fastap.js
@@ -1,12 +1,30 @@
-!function () {
-    var events = [];
+! function() {
+    var events = [],
+        touchable = isTouchDevice;
+
+    // touch event support detect
+    function isTouchDevice() {
+        try {
+            document.createEvent("TouchEvent");
+            return true;
+        } catch (e) {
+            return false;
+        }
+    }
+
     /**
      *  覆盖zepto默认的tap
      **/
     $.fn.tap = function(handler) {
         var $con = this;
 
-        function tapHandler (event) {
+        if (!touchable) {
+            $con.on('click', handler);
+            return;
+        }
+
+        // support touch event
+        function tapHandler(event) {
             var $tar = $(event.target);
             if (!$tar) return;
 
@@ -53,21 +71,27 @@
     /**
      *  取消监听tap事件（必需为覆盖zepto的tap事件）
      **/
-    $.fn.tapOff = function (handler) {
+    $.fn.tapOff = function(handler) {
 
-        for (var i = 0; i < events.length; i ++) {
+        if (!touchable) {
+            this.of('click', handler);
+            return;
+        }
+
+        // touch events off
+        for (var i = 0; i < events.length; i++) {
             var evtObj = events[i];
-                isMatched = false;
+            isMatched = false;
 
             if (evtObj) {
-                this.each(function (index, el) {
-                   evtObj.$el.each(function (index, target) {
-                       if (target == el) {
+                this.each(function(index, el) {
+                    evtObj.$el.each(function(index, target) {
+                        if (target == el) {
                             isMatched = true;
                             return true;
-                       }
-                   });
-                   if (isMatched) return true;
+                        }
+                    });
+                    if (isMatched) return true;
                 });
                 if (handler == evtObj.handler && isMatched) {
                     this.off('touchstart', evtObj.listener);
