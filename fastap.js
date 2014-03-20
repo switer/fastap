442f2915111e1462d5ea95864c2768f4e0826504
switer <guankaishe@gmail.com>
2014-03-20 15:48:09 +0800

    update fastop form my business project and add test files

--- fastap.js
+++ fastap.js
@@ -1,5 +1,7 @@
 ! function() {
-    var events = [],
+    var eventManger = {
+            events: []
+        },
         touchable = isTouchDevice;
 
     // touch event support detect
@@ -12,20 +14,45 @@
         }
     }
 
+    // Add scroll forzen time when using touch simulate click
+    if (touchable) {
+
+        var isScroll = false,
+            timeoutTimer;
+
+        function scrollHandler () {
+            // 标识为出于滚动冷冻期
+            isScroll = true;
+            // 清除上次滚动冷冻timer
+            clearTimeout(timeoutTimer);
+
+            timeoutTimer = setTimeout(function() {
+                isScroll = false;
+            }, 300);
+        }
+    }
+
+    $(window).on('scroll', scrollHandler);
+
     /**
      *  覆盖zepto默认的tap
      **/
     $.fn.tap = function(handler) {
-            var $con = this;
+        var $con = this;
 
-            if (!touchable) {
-                $con.on('click', handler);
-                return;
-            }
+        if (!touchable) {
+            $con.on('click', handler);
+            return;
+        }
 
-            // support touch event
-            function tapHandler(event) {
-                var $tar = $(event.target);
+        // support touch event
+
+        function tapHandler(event) {
+            var $tar = $(event.target),
+                ctx = {
+                    hash: window.location.hash
+                };
+
             // 解决input元素blur时不触发touchend的问题
             if (document.activeElement && document.activeElement !== event.target) {
                 document.activeElement.blur();
@@ -34,76 +61,69 @@
             var backupEvent = event.targetTouches && event.targetTouches[0] ? event: null;
             
             var oevt = event.originalEvent || backupEvent,
-                // accordType = event.originalEvent ? 'page': 'screen',
-                accordType = 'page',
                 touches = oevt && oevt.targetTouches && oevt.targetTouches[0],
-                pageX = touches ? touches[accordType + 'X'] : 0,
-                pageY = touches ? touches[accordType + 'Y'] : 0,
+                pageX = touches ? touches['pageX'] : 0,
+                pageY = touches ? touches['pageY'] : 0,
                 endTouches,
                 moveOevt,
-                endPageX, 
-                endPageY;
-
-
-            $con.off('touchmove', moveHandler);
-            $tar.off('touchend', endHandler);
-            $tar.off('scroll', scrollHandler);
+                startScrollTop = document.body.scrollTop,
+                endScrollTop = startScrollTop,
+                endPageX = 0, 
+                endPageY = 0;
             
             var isMove = false,
-                isEnd = false,
                 isFeed = false,
                 isTimeout = false,
-                isScroll = false,
-                delay = 50;
+                delay = 40; // 此值与滚动触发点击的灵敏度和点击透传的出现几率有关
 
-            function scrollHandler () {
-                isScroll = true;
-
-                setTimeout(function() {
-                    isScroll = false;
-                }, 500);
-            }
-
+            // 获取touchmove的偏移地址
             function moveHandler(e) {
                 isMove = true;
                 backupEvent = e.targetTouches && e.targetTouches[0] ? e: null;
 
                 moveOevt = e.originalEvent || backupEvent;
                 endTouches = moveOevt && moveOevt.targetTouches && moveOevt.targetTouches[0];
-                endPageX = endTouches ? endTouches[accordType + 'X'] : 0;
-                endPageY = endTouches ? endTouches[accordType + 'Y'] : 0;
+                endPageX = endTouches ? endTouches['pageX'] : 0;
+                endPageY = endTouches ? endTouches['pageY'] : 0;
             }
 
+            // touch结束的处理
             function endHandler(e) {
-                isEnd = true;
+
                 $con.off('touchmove', moveHandler);
                 $tar.off('touchend', endHandler);
-                $tar.off('scroll', scrollHandler);
+                
+                if ((!oevt || !moveOevt) && isScroll) {
+                    return;
+                }
 
-                if ((!oevt || !moveOevt) && isMove) {
-                    return;
-                }
                 // 滚动了或者偏移超过5px都取消点击行为
-                if (Math.abs(pageX - endPageX) > 5 || Math.abs(pageY - endPageY) > 5) {
+                if ( isMove && (Math.abs(pageX - endPageX) > 5 || Math.abs(pageY - endPageY) > 5) ) {
                     return;
                 }
+
                 setTimeout(function() {
-                    !isTimeout && !isScroll && isFeed && handler && handler(event);
-                }, 60);
+                    !isTimeout && !isScroll && isFeed && handler && handler(event, ctx);
+                }, 40);
+
             }
+
             $con.on('touchmove', moveHandler);
             $tar.on('touchend', endHandler);
-            $tar.on('scroll', scrollHandler);
 
+            // 此延时是为了解决滚动点击问题
             setTimeout(function() {
                 isFeed = true;
             }, delay);
 
+            // touchstart停留超过350ms认为取消点击行为
             setTimeout(function () {
                 isTimeout = true;
             }, 350);
         }
-        events.push({
+
+        // observer pattern, this is a subject
+        eventManger.events.push({
             $el: this,
             listener: tapHandler,
             handler: handler
@@ -114,17 +134,17 @@
     /**
      *  取消监听tap事件（必需为覆盖zepto的tap事件）
      **/
-    $.fn.tapOff = function(handler) {
+    $.fn.tapOff = function(evtHandler) {
 
         if (!touchable) {
-            this.of('click', handler);
+            this.of('click', evtHandler);
             return;
         }
-
         // touch events off
-        for (var i = 0; i < events.length; i++) {
-            var evtObj = events[i];
-            isMatched = false;
+        var events = [];
+        for (var i = 0; i < eventManger.events.length; i++) {
+            var evtObj = eventManger.events[i],
+                isMatched = false;
 
             if (evtObj) {
                 this.each(function(index, el) {
@@ -136,11 +156,14 @@
                     });
                     if (isMatched) return true;
                 });
-                if (handler == evtObj.handler && isMatched) {
+                if ( ( !evtHandler || evtHandler === evtObj.handler) && isMatched) {
                     this.off('touchstart', evtObj.listener);
-                    events[i] = null;
+                    continue;
                 }
+                
             }
+            events.push(evtObj);
         }
+        eventManger.events = events;
     };
 }();
